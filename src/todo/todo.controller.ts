5ca6ff3c0e5ac2f82c440f70d845ab6d0dcc001e
dimchik_st@list.ru <dimchik_st@list.ru>
2022-08-09 04:23:36 +0300

    Add destroy Todo route

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { TodoService } from './todo.service';
@@ -15,5 +15,10 @@ export class TodoController {
   @Post()
   store(@Body() dto: CreateTodoDto) {
     return this.todoService.save(dto);
+  }
+
+  @Delete(':id')
+  destroy(@Param('id') id: string) {
+    return { test: +id };
   }
 }
